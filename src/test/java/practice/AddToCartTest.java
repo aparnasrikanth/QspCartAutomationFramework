653cb14862b4d9c34508ca11b96c279c7661242a
DELL <DELL@DESKTOP-MLOMP2I>
2022-09-13 23:04:52 +0530

    First Commit by engineer 1-addToCart

---
+++ src/test/java/practice/AddToCartTest.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class AddToCartTest {
+	@Test
+	public void addToCartTest() {
+		System.out.println("Product Added");
+		
+	}
+
+}
